15d3208affe9508766f597f24ce9dc8c025e748e
Shunya Kitada <syun.kitada@gmail.com>
2015-08-30 02:37:18 +0900

    updated

--- rabbitmq.py
+++ rabbitmq.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 
-from fabkit import sudo
+import socket
+from fabkit import sudo, filer, env, Editor, api
 from fablib.base import SimpleBase
 
 
@@ -22,27 +23,70 @@ class RabbitMQ(SimpleBase):
             ]
         }
 
+    def init_data(self):
+        for cluster in self.data.values():
+            if env.host in cluster['hosts']:
+                self.data['cluster'] = cluster
+                break
+
     def setup(self):
-        data = self.init()
+        self.init()
+
+        cluster = self.data['cluster']
+
         self.install_packages()
+
+        filer.template('/var/lib/rabbitmq/.erlang.cookie',
+                       src_str=cluster['cookie'],
+                       mode='400',
+                       owner='rabbitmq:rabbitmq')
+
+        etc_hosts = Editor('/etc/hosts')
+        for i, host in enumerate(cluster['hosts']):
+            ip = socket.gethostbyname(host)
+            etc_hosts.a('{0} rabbit{1}'.format(ip, i))
+
+        node_index = cluster['hosts'].index(env.host)
+        nodename = 'rabbit@rabbit{0}'.format(node_index)
+        filer.template('/etc/rabbitmq/rabbitmq-env.conf',
+                       src_str='NODENAME={0}'.format(nodename),
+                       mode='644',
+                       owner='rabbitmq:rabbitmq')
+
         self.enable_services().start_services()
-        sudo('rabbitmqctl status')
+
         sudo('rabbitmq-plugins enable rabbitmq_management')
 
-        result = sudo('rabbitmqctl list_vhosts | grep -v Listing')
-        vhosts = result.split('\r\n')
-        for vhost in data['vhosts'].values():
-            if vhost not in vhosts:
-                sudo('rabbitmqctl add_vhost {0}'.format(vhost))
+        if nodename == 'rabbit@rabbit0':
+            result = sudo('rabbitmqctl list_vhosts | grep -v Listing')
+            vhosts = result.split('\r\n')
+            for vhost in cluster['vhosts'].values():
+                if vhost not in vhosts:
+                    sudo('rabbitmqctl add_vhost {0}'.format(vhost))
 
-        result = sudo('rabbitmqctl list_users | grep -v Listing | awk \'{print $1}\'')
-        users = result.split('\r\n')
-        if 'guest' in users:
-            sudo('rabbitmqctl delete_user guest')
+            result = sudo('rabbitmqctl list_users | grep -v Listing | awk \'{print $1}\'')
+            users = result.split('\r\n')
+            if 'guest' in users:
+                sudo('rabbitmqctl delete_user guest')
 
-        for user in data['users'].values():
-            if user['user'] not in users:
-                sudo('rabbitmqctl add_user {0[user]} {0[password]}'.format(user))
-            for permission in user['permissions']:
-                sudo('rabbitmqctl set_permissions -p {1[vhost]} {0[user]} {1[permissions]}'.format(
-                    user, permission))
+            for user in cluster['users'].values():
+                if user['user'] not in users:
+                    sudo('rabbitmqctl add_user {0[user]} {0[password]}'.format(user))
+                for permission in user['permissions']:
+                    sudo('rabbitmqctl set_permissions -p {1[vhost]} {0[user]} {1[permissions]}'.format(user, permission))  # noqa
+
+    def setup_cluster(self):
+        self.init()
+        cluster = self.data['cluster']
+
+        if env.host == cluster['hosts'][0]:
+            for vhost in cluster['vhosts'].values():
+                sudo('rabbitmqctl set_policy all \'^.*\' \'{{"ha-mode": "all"}}\' -p {0}'.format(vhost))  # noqa
+        else:
+            with api.warn_only():
+                result = sudo('rabbitmqctl cluster_status | grep rabbit@rabbit0')
+            if result.return_code != 0:
+                sudo('rabbitmqctl stop_app')
+                sudo('rabbitmqctl reset')
+                sudo('rabbitmqctl join_cluster rabbit@rabbit0')
+                sudo('rabbitmqctl start_app')
